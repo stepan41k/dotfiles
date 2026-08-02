bc021f8c695e025dec161fa65401f42a28644172
stepan41k <russia.zont@gmail.com>
2026-08-02 21:58:17 +0300

    fix(os/fedora): change script, fix set command

--- os/fedora/shortcuts.sh
+++ os/fedora/shortcuts.sh
@@ -3,14 +3,19 @@
 CONF_FILE="shortcuts.conf"
 
 if [ ! -f "$CONF_FILE" ]; then
-    echo "Error: file $CONF_FILE not found!"
+    echo "ERROR: file $CONF_FILE not found!"
     exit 1
 fi
 
 dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < "$CONF_FILE"
 
-SLOTS=$(grep -oP '(?<=^\[)custom[^\]]+' "$CONF_FILE" | sed "s#^#'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/#; s#\$#/#'" | paste -sd, -)
+SLOTS=""
+for section in $(grep -E '^\[custom' "$CONF_FILE" | tr -d '[]'); do
+    SLOTS="${SLOTS}'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/${section}/', "
+done
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[$SLOTS]"
+SLOTS="[${SLOTS%, }]"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$SLOTS"
 
 echo "Shortcuts from $CONF_FILE successfully installed"
